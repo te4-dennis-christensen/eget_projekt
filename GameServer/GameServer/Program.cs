f2da8de9f33cae9b1e573067fa3cc51b5899b6ae
Dennis-coder <54890763+Dennis-coder@users.noreply.github.com>
2020-11-06 23:02:52 +0100

    s

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GameServer
+{   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Title = "GameServer";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
