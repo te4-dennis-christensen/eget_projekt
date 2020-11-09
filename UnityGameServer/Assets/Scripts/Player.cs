6542765f7adb927cb5de10f8b0580c0026166adb
Te4-Dennis-Christensen <dennis.christensen@elev.ga.ntig.se>
2020-11-09 14:41:38 +0100

    for test in windows

---
+++ UnityGameServer/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int id;
+    public string username;
+
+    private float moveSpeed = 5f / Constants.TICKS_PER_SECOND;
+    private bool[] inputs;
+
+    public void Initialize(int _id, string _username)
+    {
+        id = _id;
+        username = _username;
+
+        inputs = new bool[4];
+    }
+
+    public void Update()
+    {
+        Vector2 _inputDirection = Vector2.zero;
+        if (inputs[0])
+        {
+            _inputDirection.y += 1;
+        }
+        if (inputs[1])
+        {
+            _inputDirection.x += 1;
+        }
+        if (inputs[2])
+        {
+            _inputDirection.y -= 1;
+        }
+        if (inputs[3])
+        {
+            _inputDirection.x -= 1;
+        }
+
+        Move(_inputDirection);
+    }
+
+    private void Move(Vector2 _inputDirections)
+    {
+        Vector3 _moveDirection = transform.right * _inputDirections.x + transform.forward * _inputDirections.y;
+        transform.position += _moveDirection * moveSpeed;
+
+        ServerSend.PlayerPosition(this);
+        ServerSend.PlayerRotation(this);
+    }
+
+    public void SetInput(bool[] _inputs, Quaternion _rotation)
+    {
+        inputs = _inputs;
+        transform.rotation = _rotation;
+    }
+}
